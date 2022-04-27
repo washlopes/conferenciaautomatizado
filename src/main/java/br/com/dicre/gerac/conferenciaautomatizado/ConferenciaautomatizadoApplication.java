45d0c472b3c7aa9255273a657940b6ad39279bd2
Washington Luís Pedrosa Lopes <washlopes@gmail.com>
2022-04-27 15:31:48 -0300

    Criação projeto conferencia automatizado!

---
+++ src/main/java/br/com/dicre/gerac/conferenciaautomatizado/ConferenciaautomatizadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.dicre.gerac.conferenciaautomatizado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConferenciaautomatizadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConferenciaautomatizadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/dicre/gerac/conferenciaautomatizado/ConferenciaautomatizadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dicre.gerac.conferenciaautomatizado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenciaautomatizadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
